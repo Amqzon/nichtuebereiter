5e5558bc18380773b01bd9b4124492f6bb12970a
Snow <snowasdev@gmail.com>
2021-04-23 17:10:29 +0200

    made it pretty
    wonderful

--- reiterinterpreter.py
+++ reiterinterpreter.py
@@ -1,55 +1,37 @@
-import random
-
-überreiter = {
-    "zeigan":print,
-    "zufall":random.randint,
-    "mathe":eval,
-    "schreibe":eval,
-    "liste":eval
-    }
-
-filename = "ueber.reiter"
-
-with open(filename) as f:
-    content = f.readlines()
-
-for line in content:
-    
-    x = line.split("_")
-    
-    if line.startswith("premiumzeigan"):
-        x2 = line.split(";")
-        varinput = x2[1].split("{")
-        eingabe = ""
-        for i in varinput[1]:
-            if i == "}":
-                break
-            else:
-                eingabe = eingabe + i
-        if x2[0].endswith("zufall"):
-            zufallinput = eingabe.split(",")
-            zffer = (varinput[0] + str(random.randint(int(zufallinput[0]),int(zufallinput[1]))))
-            print(zffer)
-
-        if x2[0].endswith("mathe"):
-            print(varinput[0] + str(eval(eingabe)))
-            
-    
-    if line.startswith("zeigan"):
-        überreiter[x[0]](x[1])
-
-    if line.startswith("zufall"):
-        x2 = x[1].split(",")
-        print(überreiter[x[0]](int(x2[0]),int(x2[1])))
-    
-    if line.startswith("mathe"):
-        ergebnis = überreiter[x[0]]("print(" + x[1]+ ")")
-    
-    if line.startswith("schreibe"):
-        speicherarg = x[1].split(";")
-        f = open(speicherarg[0],speicherarg[1])
-        f.write(speicherarg[2])
-        f.close()
-    if line.startswith("liste"):
-        trennung = x[1]
-        reiterliste = überreiter[x[0]]
+import random #line:1
+überreiter ={"zeigan":print ,"zufall":random .randint ,"mathe":eval ,"schreibe":eval ,"liste":eval }#line:9
+filename ="ueber.reiter"#line:11
+with open (filename )as f :#line:13
+    content =f .readlines ()#line:14
+for line in content :#line:16
+    x =line .split ("_")#line:18
+    if line .startswith ("premiumzeigan"):#line:20
+        x2 =line .split (";")#line:21
+        varinput =x2 [1 ].split ("{")#line:22
+        eingabe =""#line:23
+        for i in varinput [1 ]:#line:24
+            if i =="}":#line:25
+                break #line:26
+            else :#line:27
+                eingabe =eingabe +i #line:28
+        if x2 [0 ].endswith ("zufall"):#line:29
+            zufallinput =eingabe .split (",")#line:30
+            zffer =(varinput [0 ]+str (random .randint (int (zufallinput [0 ]),int (zufallinput [1 ]))))#line:31
+            print (zffer )#line:32
+        if x2 [0 ].endswith ("mathe"):#line:34
+            print (varinput [0 ]+str (eval (eingabe )))#line:35
+    if line .startswith ("zeigan"):#line:38
+        überreiter [x [0 ]](x [1 ])#line:39
+    if line .startswith ("zufall"):#line:41
+        x2 =x [1 ].split (",")#line:42
+        print (überreiter [x [0 ]](int (x2 [0 ]),int (x2 [1 ])))#line:43
+    if line .startswith ("mathe"):#line:45
+        ergebnis =überreiter [x [0 ]]("print("+x [1 ]+")")#line:46
+    if line .startswith ("schreibe"):#line:48
+        speicherarg =x [1 ].split (";")#line:49
+        f =open (speicherarg [0 ],speicherarg [1 ])#line:50
+        f .write (speicherarg [2 ])#line:51
+        f .close ()#line:52
+    if line .startswith ("liste"):#line:53
+        trennung =x [1 ]#line:54
+        reiterliste =überreiter [x [0 ]]
